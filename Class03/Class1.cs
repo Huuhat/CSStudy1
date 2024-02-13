3ad6d14899cd5eceefc0472ca7bd8d0362e4a60e
Huuhat <wjdehddud23@naver.com>
2024-02-13 17:04:23 +0900

    (2023.02.13) 숫자맞추기 게임 풀이 및 문자열 처리방법

---
+++ Class03/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class03
+{
+    internal class 학습_목표
+    {
+        // 3강
+        // 날짜 : 2024-02-13
+        // 학습목표:
+        // 1. 조건문,반복문,점프문 학습
+        // 2. 문자열 처리에 대한 학습
+
+        // 조건문 if. switch
+
+
+
+
+
+
+    }
+}
